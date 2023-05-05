03dcf7d89b1317e3abbe1bab29ec4e99fee7d3a2
gupta kunal <132100570+testgithubgupta@users.noreply.github.com>
2023-05-05 23:24:57 +0530

    farenhiet.cpp

---
+++ farenhiet.cpp
@@ -0,0 +1,22 @@
+temperature in c
+38
+temperature in f=126
+  
+  
+  
+  
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float c,f;
+    cout<<" enter temperature in centrigarte";
+    cin>>c;
+    f=(9*(c+32)/5);
+    cout<<" temperature in farenhiet" << f;
+
+    return 0;
+}
