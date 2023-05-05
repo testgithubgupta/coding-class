2b6a3aab6dce5a84f51b2c9a5f372ba8a6b8fa71
gupta kunal <132100570+testgithubgupta@users.noreply.github.com>
2023-05-05 21:28:02 +0530

    simple interest .cpp

---
+++ simple interest .cpp
@@ -0,0 +1,28 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ int p,r,t,si;
+   cout<<"enter principarl rate and time";
+   cin>>p;
+   cin>>r;
+
+   cin>>t;
+   si=p*r*t/100;
+   cout<<" simple interest " << si;
+   
+   return 0;
+    
+}
+    
+
+    
+
+calculate simple interest
+p=1500
+r=4
+t=2
+  si= 120
