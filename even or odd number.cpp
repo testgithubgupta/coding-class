23e47d909f627cf574077a814205efcd88e0e181
gupta kunal <132100570+testgithubgupta@users.noreply.github.com>
2023-05-05 23:51:05 +0530

    even or odd number.cpp

---
+++ even or odd number.cpp
@@ -0,0 +1,33 @@
+enter number
+2
+even
+enter number
+7 
+  odd
+  
+ 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<" enter  number";
+    cin>>n;
+    if(n%2==0)
+    {
+        cout<<" even";
+        
+    }
+    else
+    {
+        cout<<" odd";
+        
+    }
+    
+    return 0;
+
+    
+}
