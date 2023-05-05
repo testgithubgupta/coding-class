d0b3feafad441298ae30ed734b457f2a310b62a9
gupta kunal <132100570+testgithubgupta@users.noreply.github.com>
2023-05-05 23:41:03 +0530

    equal number or not.cpp

---
+++ equal number or not.cpp
@@ -0,0 +1,32 @@
+enter two number
+2
+
+2
+number are equal
+2
+3
+number are not equal
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<" enter two number";
+    cin>>a;
+    cin>>b;
+    if(a==b)
+    {
+        cout<<"number are  equal";
+        
+    }
+    else
+    {
+        cout<<" number are  not equal";
+    }
+
+    return 0;
+}
